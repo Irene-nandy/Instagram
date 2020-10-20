18436b0577d29feae4d8e12cb1355a279b575093
Irene-nandy <irenekasiva75@gmail.com>
2020-10-20 16:55:34 +0300

    update change

--- instagramUsers/apps.py
+++ instagramUsers/apps.py
@@ -1,8 +1,8 @@
-# # -*- coding: utf-8 -*-
-# from __future__ import unicode_literals
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-# from django.apps import AppConfig
+from django.apps import AppConfig
 
 
-# class InstagramusersConfig(AppConfig):
-#     name = 'instagramUsers'
+class InstagramusersConfig(AppConfig):
+    name = 'instagramUsers'
