c23d9f1ee27068464dcd99545d550c304f0ced07
Irene-nandy <irenekasiva75@gmail.com>
2020-10-20 16:43:08 +0300

    add apps

--- instagramHome/apps.py
+++ instagramHome/apps.py
@@ -1,8 +1,8 @@
-# # -*- coding: utf-8 -*-
-# from __future__ import unicode_literals
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-# from django.apps import AppConfig
+from django.apps import AppConfig
 
 
-# class InstagramhomeConfig(AppConfig):
-#     name = 'instagramHome'
+class InstagramhomeConfig(AppConfig):
+    name = 'instagramHome'
